26a1b9bc035db264158c8c97f48c8a76dd658a6f
Varun kumar <pro@MACs-MacBook-Pro.local>
2022-10-22 14:50:55 +0530

    initial commit

---
+++ Logger.js
@@ -0,0 +1,111 @@
+// var url = 'facebook.com';
+// //console.log(__filename);
+// //console.log(__dirname);
+// function log(message)
+// {
+//     console.log(message);
+// }
+// module.exports.log = log;
+// module.exports.endPoint = url;
+
+
+// //const express =  require('express');
+ 
+
+
+// //function sayHello(name)
+// //{
+//  //   console.log('Hello ' + name);
+// //}
+// //sayHello('Macuser');
+// //var con = require('./logger');
+// //con.log('Access from  index.js');
+// //path module
+// /*
+// const path = require('path');
+
+// var pathObj = path.parse(__filename); 
+
+// console.log(pathObj);
+// */
+// //Usage of operating system module
+
+// /*
+// const os = require('os');
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
+
+// console.log('Total memory :' + totalMemory);
+// console.log('Free memory :' + freeMemory);
+// */
+
+// //usage of file system module
+
+// /*
+// const fs = require('fs');
+// //const fileNames = fs.readdirSync('./');
+// //console.log(fileNames);
+
+// fs.readdir('./', function(err, files)
+// {
+//     if(err) console.log('Error', err);
+//     else console.log('Result', files)
+
+// }
+// );
+// */
+
+// /*
+// //usage of modules like events.
+// const EventEmitter = require('events'); 
+// const emitter = new EventEmitter();
+// emitter.on('check', function()
+// {
+//     console.log('checked_login_event');
+
+// });
+  
+// emitter.emit('check');
+// */
+
+// /*
+// const EventEmitter = require('events');
+// const emitter = new EventEmitter();
+
+// //listener checking data
+// emitter.on('DataChecking', function(arg)
+// {
+//     console.log('Listener checking data', arg);
+// }
+// );
+
+// //raising event
+// emitter.emit('DataChecking', {id : 1, url: 'http://'});
+// */
+
+// /*
+// const http = require('http');
+
+// const server = http.createServer((req,res) => {
+//     if(req.url === '/')
+//     {
+//          res.write('Hello world');
+//          res.end();  
+//     }
+// });
+
+// /*
+// server.on('connection', (socket) => {
+// console.log('New connection .....');
+// });
+// */
+// /*
+// server.listen(3000);
+// console.log('listening port 3000.....');
+// */
+
+
+
+// /*import express from 'express';
+// const express =  require('express');
+// */
